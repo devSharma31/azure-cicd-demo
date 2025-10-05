613f4683e7a3d506275eccbd2b664d40b9e514cc
Devashish Sharma <87937859+devSharma31@users.noreply.github.com>
2025-10-05 16:34:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def root():
     return "Hello from Devashish's Azure CI/CD demo! 🚀"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+@app.route("/health")
+def health():
+    return jsonify(ok=True)
